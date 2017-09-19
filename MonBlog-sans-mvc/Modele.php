8f0771ae12cc7a07d68b098e324344879eb969e2
Margoulino <ludovic.mahe0@gmail.com>
2017-09-19 16:19:11 +0200

    ajout getBbd

--- MonBlog-sans-mvc/Modele.php
+++ MonBlog-sans-mvc/Modele.php
@@ -2,7 +2,12 @@
 
 // Renvoie la liste de tous les billets, triés par identifiant décroissant
 function getBillets() {
-	$bdd = new PDO('mysql:host=localhost;dbname=mvcBlog;charset=utf8', 'ts2', 'ts2');
+	$bdd = getBdd();
 	$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' order by BIL_ID desc');
 	return $billets;
+}
+
+function getBdd(){
+	$bdd = new PDO('mysql:host=localhost;dbname=mvcBlog;charset=utf8', 'ts2', 'ts2');
+	return $bdd;
 }
